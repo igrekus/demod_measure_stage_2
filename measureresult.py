628f994113be558ce354e6089f15c73192a7a817
igrekus <upload234@mail.ru>
2021-05-19 15:31:00 +0300

    FEAT: implement adjustment logic

--- measureresult.py
+++ measureresult.py
@@ -1,5 +1,7 @@
+import ast
 import datetime
 import os.path
+import pprint
 
 from collections import defaultdict
 from subprocess import Popen
@@ -24,6 +26,11 @@ class MeasureResult:
 
         self.data = defaultdict(list)
 
+        self.adjustment = None
+        if os.path.isfile('./adjust.ini'):
+            with open('./adjust.ini', 'rt', encoding='utf-8') as f:
+                self.adjustment = ast.literal_eval(''.join(f.readlines()))
+
     def __bool__(self):
         return self.ready
 
@@ -42,6 +49,10 @@ class MeasureResult:
         p_loss = data['loss']
         k_loss = p_pch - p_rf + p_loss
         # endregion
+
+        if self.adjustment is not None:
+            point = self.adjustment[len(self._processed)]
+            k_loss += point['k_loss']
 
         self._report = {
             'p_lo': p_lo,
@@ -74,6 +85,20 @@ class MeasureResult:
     def add_point(self, data):
         self._raw.append(data)
         self._process_point(data)
+
+    def save_adjustment_template(self):
+        if self.adjustment is None:
+            print('measured, saving template')
+            self.adjustment = [{
+                'p_lo': p['p_lo'],
+                'f_lo': p['f_lo'],
+                'p_rf': p['p_rf'],
+                'f_rf': p['f_rf'],
+                'k_loss': 0,
+
+            } for p in self._processed]
+        with open('adjust.ini', mode='wt', encoding='utf-8') as f:
+            pprint.pprint(self.adjustment, stream=f, sort_dicts=False)
 
     @property
     def report(self):
